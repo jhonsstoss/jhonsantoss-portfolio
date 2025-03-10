962a57fe532069e8d06f378d60966bb1960a3904
Jhonatas Santos <grigorio.ssantos@gmail.com>
2025-03-10 15:24:05 -0300

    added new sites

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -18,10 +18,10 @@ export const Projects = () => {
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)]">
             <h3 className="text-xl font-bold mb-2">DrawGenAi</h3>
             <p className="text-gray-400 mb-4">
-              Scalable cloud infrasctructure management with real-time monitoring and automated scaling.
+              A SaaS platform for AI-powered image generation, allowing users to create high-quality visuals effortlessly. It features a complete authentication system with login, registration, and payment integration, ensuring a seamless and secure user experience.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {["Vite", "Node.js", "JavaScript", "React", "TailWindCSS", "MongoDB", "APIrest", "AI", "Render", "Motion Frame"].map((tech, key) => (
+              {["React", "Vite", "Node.js", "JWT Auth", "Axios", "Tailwind CSS", "MongoDB", "API'", "Motion"].map((tech, key) => (
                 <span 
                   key={key}
                   className="bg-blue-500/10 text-blue-500 
@@ -44,7 +44,7 @@ export const Projects = () => {
           </div>
           
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)]">
-            <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
+            <h3 className="text-xl font-bold mb-2">Messenger</h3>
             <p className="text-gray-400 mb-4">
               Scalable cloud infrasctructure management with real-time monitoring and automated scaling.
             </p>
@@ -100,12 +100,12 @@ export const Projects = () => {
           </div>
           
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)]">
-            <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
+            <h3 className="text-xl font-bold mb-2">Rick and Morty Characters</h3>
             <p className="text-gray-400 mb-4">
-              Scalable cloud infrasctructure management with real-time monitoring and automated scaling.
+              A web application that allows users to explore characters from the Rick and Morty universe. Using the official API, the site dynamically fetches and displays detailed information about each character, including their status, species, and origin, in a simple and engaging interface.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+              {["HTML", "CSS", "JavaScript", "REST API"].map((tech, key) => (
                 <span 
                   key={key}
                   className="bg-blue-500/10 text-blue-500 
@@ -118,7 +118,65 @@ export const Projects = () => {
             </div>
             <div className="flex justify-between items-center">
               <a 
-                href="#" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                href="https://rick-morty-characters-beta.vercel.app/" 
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                {""}View Project →{""}
+              </a>
+            </div>
+          </div>
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)]">
+            <h3 className="text-xl font-bold mb-2">Oppenheimer Project</h3>
+            <p className="text-gray-400 mb-4">
+              A web application dedicated to the Oppenheimer movie, featuring an interactive experience with the official trailer, synopsis, streaming availability, cast details, and a comment section for user discussions. The site provides a seamless navigation experience with a modern UI and dynamic content rendering.
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["React", "React Router DOM", "Bootstrap ", "CSS", "JavaScript", "Swiper"].map((tech, key) => (
+                <span 
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 
+                  py-1 px-3 rounded-full text-sm 
+                  hover:bg-blue-500/20 
+                  hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className="flex justify-between items-center">
+              <a 
+                target="_blank" 
+                rel="noopener noreferrer"
+                href="https://rick-morty-characters-beta.vercel.app/" 
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                {""}View Project →{""}
+              </a>
+            </div>
+          </div>
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)]">
+            <h3 className="text-xl font-bold mb-2">Rick and Morty Characters</h3>
+            <p className="text-gray-400 mb-4">
+              A web application that allows users to explore characters from the Rick and Morty universe. Using the official API, the site dynamically fetches and displays detailed information about each character, including their status, species, and origin, in a simple and engaging interface.
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["HTML", "CSS", "JavaScript", "REST API"].map((tech, key) => (
+                <span 
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 
+                  py-1 px-3 rounded-full text-sm 
+                  hover:bg-blue-500/20 
+                  hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className="flex justify-between items-center">
+              <a 
+                target="_blank" 
+                rel="noopener noreferrer"
+                href="https://rick-morty-characters-beta.vercel.app/" 
                 className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                 {""}View Project →{""}
               </a>
