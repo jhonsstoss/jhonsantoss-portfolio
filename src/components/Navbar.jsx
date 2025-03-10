26f47ad4d5e7898569351445b7f789b2bda71ee6
Jhonatas Santos <grigorio.ssantos@gmail.com>
2025-03-10 18:07:13 -0300

    language

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import styles from "./Navbar.css"
 
 export const Navbar = ({menuOpen, setMenuOpen}) => {
 
@@ -21,25 +22,38 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
           <div className="hidden md:flex items-center space-x-8">
             <a 
             href="#home" 
-            className="text-gray-300 hove:text-white transition-colors">
+            className="text-gray-300 hover:text-white transition-colors">
               {" "}Home{" "}
             </a>
             <a 
             href="#about" 
-            className="text-gray-300 hove:text-white transition-colors">
+            className="text-gray-300 hover:text-white transition-colors">
               {" "}About{" "}
             </a>
             <a 
             href="#projects" 
-            className="text-gray-300 hove:text-white transition-colors">
+            className="text-gray-300 hover:text-white transition-colors">
               {" "}Projects{" "}
             </a>
             <a 
             href="#contact" 
-            className="text-gray-300 hove:text-white transition-colors">
+            className="text-gray-300 hover:text-white transition-colors">
               {" "}Contact{" "}
             </a>
           </div>
+
+          <div className="language">
+            <div className="language-selected">
+
+            </div>
+            <ul>
+              <li><a href="" className="br"></a>Português</li>
+              <li><a href="" className="en">English</a></li>
+              <li><a href="" className="es">Español</a></li>
+            </ul>
+          </div>
+
+          <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         </div>
       </div>
     </nav>
